fe5c01270bcd8d255e2ed41f10f8b8adc78f2994
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-11-02 10:48:04 +0100

    WIP: add loop for every single project
    in loading from github

--- app.js
+++ app.js
@@ -6,17 +6,49 @@
 
 
 
-var resultDiv = document.getElementById('result');
+var resultDiv = document.getElementsByClassName('result');
 var url = 'https://api.github.com/users/zdenekpribyla/repos';
 fetch(url)
     .then((resp) => resp.json())
 .then(function(data) {
-    data.forEach(function (project) {
+    console.log(data);
 
-        resultDiv.append('<h1>' + project.name + '</h1>');
-        resultDiv.append('<p>' + project.description + '</p>');
-        resultDiv.append('<hr>')
-    })
+        var onlyName = data.map(function (project) {
+        return project.name;
+            });
+    console.log(onlyName);
+
+    onlyName.forEach(function (singleProject) {
+        var el = document.getElementById('result'),
+            // Make a new div
+            elChild = document.createElement("h2");
+
+// Give the new div some content
+        elChild.innerHTML = singleProject;
+
+// Chug in into the parent element
+        el.appendChild(elChild);
+    });
+
+
+
+    // resultDiv.innerHTML = ('<h1>' + onlyName + '</h1>');
+    // console.log(resultDiv.innerHTML = '<h1>' + onlyName + '</h1>');
+
+    // var projectN = data.map(function (name) {
+    //     return name.projectName
+    //
+    // });
+    // console.log(projectN);
+    // data.forEach(function (project) {
+    //     var projectName = project.name;
+    //     resultDiv.innerHTML = ('<h1>' + projectName + '</h1>');
+    //     // resultDiv.innerHTML('<p>' + project.description + '</p>');
+    //     // resultDiv.innerHTML('<hr>')
+    //     resultDiv.append('<h1>' + project.name + '</h1>');
+    //     resultDiv.append('<p>' + project.description + '</p>');
+    //     resultDiv.append('<hr>')
+    // })
 
 });
     // .catch(function(error) {
