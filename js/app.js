277941dbb1b8064c50ee39fcfdeb3d4c99d2dfb7
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-11-08 16:02:18 +0100

    code cleanup

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
-//var _ = require('lodash');
-//
-// var testApp = function () {
-//     alert('hi man')
-// };
 
-
-$(document).ready(function(){
+$(document).ready(function () {
 
     console.log('jQuery ready');
 
@@ -23,7 +17,6 @@ fetch(url)
     data.forEach(function (project) {
 
 
-
         var parentElement = document.getElementById('result');
 
         var newCreatedElement = document.createElement("div");
@@ -37,7 +30,7 @@ fetch(url)
 
     });
     var buttonsClass = document.getElementsByClassName('button-class');
-    for(i = 0; i < buttonsClass.length; i++) {
+    for (i = 0; i < buttonsClass.length; i++) {
         buttonsClass[i].addEventListener('click', function () {
             console.log('klik funguje');
 
@@ -46,74 +39,5 @@ fetch(url)
 
 
 });
-
-
-
-
-// var projectDetailElement = document.getElementsByClassName('projects__details');
-// for(var index = 0; index < projectDetailElement.length; index++) {
-//     var singleDetail = projectDetailElement[index];
-//     var isDetailsVisible = false;
-//     var readDetails = function () {
-//         if (isDetailsVisible) {
-//
-//             singleDetail.style.display = 'none';
-//             isDetailsVisible = false
-//         }
-//         else {
-//
-//             singleDetail.style.display = 'block';
-//             isDetailsVisible = true
-//         }
-//     };
-// }
-//
-// var buttonsClass = document.getElementsByClassName('button-class');
-// for(i = 0; i < buttonsClass.length; i++) {
-//     buttonsClass[i].addEventListener('click', function () {
-//         console.log('klik funguje');
-//         readDetails()
-//     })
-// }
-
-
-// for(i = 0; i < buttonsClass.length; i++) {
-// // buttonsClass.forEach(function (click) {
-//     buttonsClass[i].addEventListener('click', function () {
-//         readDetails()
-//     });
-//     var isDetailsVisible = false;
-//     var readDetails = function () {
-//         if (isDetailsVisible) {
-//
-//             document.getElementById('project-details').style.display = 'none';
-//             isDetailsVisible = false
-//         }
-//         else {
-//
-//             document.getElementById('project-details').style.display = 'block';
-//             isDetailsVisible = true
-//         }
-//     };
-//
-// }
-
-
-
-// var isDetailsVisible = false;
-// var readDetails = function () {
-//     if (isDetailsVisible) {
-//
-//         document.getElementById('project-details').style.display = 'none';
-//         isDetailsVisible = false
-//     }
-//     else {
-//
-//         document.getElementById('project-details').style.display = 'block';
-//         isDetailsVisible = true
-//     }
-// };
-
-
 
 
