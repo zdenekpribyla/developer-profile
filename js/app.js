0de3d89f77a64b70bf2a8150a0afa1eb1888955c
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-11-08 17:38:08 +0100

    add loading of projects via api

--- js/app.js
+++ js/app.js
@@ -8,19 +8,14 @@ $(document).ready(function () {
         data.forEach(function (project) {
 
 
-            var parentElement = document.getElementById('result');
-
-            var newCreatedElement = document.createElement('div');
-            newCreatedElement.className = "projects";
-
-            newCreatedElement.innerHTML = ('<h2 class="projects__title">' + project.name + '</h2>' +
-                '<p class="projects__description">' + project.description + '</p><div class="projects__read-more"><input type="button" value="Read Details" class="button-class"></div><div class="projects__details" id="project-details">Watch code source: ' + '<a href="' + project.html_url + '">' + project.html_url + '</div>');
-
-            parentElement.appendChild(newCreatedElement);
-
-
+            $('.projects-list').append('<div class="project"><h2 class="projects__title">' + project.name + '</h2><p class="projects__description">' + project.description +
+                '</p><div class="projects__read-more"><input type="button" value="Read Details" class="button-class"></div><div class="projects__details" id="project-details">Watch code source: ' + '<a href="' + project.html_url + '">' + project.html_url + '</div></div>')
+            
         });
+
+
     });
 
-
 });
+
+
