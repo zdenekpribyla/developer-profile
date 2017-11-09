955335b96dd3b0dd1ec3505ff983cfef098d51eb
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-11-09 17:11:49 +0100

    add font-awesome

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ $(document).ready(function () {
 
         data.forEach(function (project) {
 
-            $('.projects-list').append(
+            $('.center__list').append(
                 '<div class="project">' +
                 '<h2 class="project__title">' + project.name + '</h2>' +
                 '<p class="project__description">' + project.description + '</p>' +
-                '<button type="button" class="display-detail-btn">Open details</button>' +
+                '<button type="button" class="project__detail-btn">Open details</button>' +
                 '<div class="project__detail">Watch code source: ' +
                 '<a href="' + project.html_url + '">' + project.html_url + '</a>' +
                 '</div>' +
@@ -21,7 +21,7 @@ $(document).ready(function () {
     });
 
     // https://stackoverflow.com/questions/1359018/in-jquery-how-to-attach-events-to-dynamic-html-elements
-    $('.projects-list').on('click', '.display-detail-btn', function () {
+    $('.center__list').on('click', '.project__detail-btn', function () {
         console.log('click button');
         var projectDetail = $(this).parent().find('.project__detail');
         $(projectDetail).toggle(400);
