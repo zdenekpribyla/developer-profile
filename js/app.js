7598831809a8c4dabeab5f987512b86faea80d5b
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-11-09 18:40:38 +0100

    get local .json projects data (instead of API gitHub)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
 
-    console.log('jQuery ready');
-
-    $.get('https://api.github.com/users/zdenekpribyla/repos', function (data) {
+    $.get('./js/data.json', function (data) {
         console.log(data);
+            
 
         data.forEach(function (project) {
 
@@ -11,17 +10,16 @@ $(document).ready(function () {
                 '<div class="project">' +
                 '<h2 class="project__title">' + project.name + '</h2>' +
                 '<p class="project__description">' + project.description + '</p>' +
-                '<button type="button" class="project__detail-btn">Open details</button>' +
                 '<div class="project__detail">Watch code source: ' +
                 '<a href="' + project.html_url + '">' + project.html_url + '</a>' +
-                '</div>' +
+                '</div>' + '<div class="project__btn"><i class="fa fa-caret-square-o-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
                 '</div>')
         });
 
     });
 
     // https://stackoverflow.com/questions/1359018/in-jquery-how-to-attach-events-to-dynamic-html-elements
-    $('.center__list').on('click', '.project__detail-btn', function () {
+    $('.center__list').on('click', '.project__btn', function () {
         console.log('click button');
         var projectDetail = $(this).parent().find('.project__detail');
         $(projectDetail).toggle(400);
@@ -29,21 +27,6 @@ $(document).ready(function () {
 
 
 });
-// var isElementVisible = false;
-//
-// if (isElementVisible) {
-//     projectDetail.hide();
-//     console.log('spustil if');
-//     isElementVisible = false
-// }
-// else {
-//     projectDetail.show();
-//     console.log('spustil else');
-//     isElementVisible = true
-// }
 
 
-
-
-
-
+//https://api.github.com/users/zdenekpribyla/repos
