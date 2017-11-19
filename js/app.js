3a215ac1baaba3997a5c2fe7ddfb55cb71d8c6c5
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-11-19 15:21:02 +0100

    change naming, clean code, replacement of filterTags.click

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// var _ = require('lodash');
+
 $(document).ready(function () {
 
     $.get('./js/data.json', function (data) {
@@ -15,44 +17,41 @@ $(document).ready(function () {
                 '</div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
                 '</div>');
 
-            var filterTags = $('.filter__tag');
-            filterTags.click(function () {
-                var that = $(this);
-                if ($(this).hasClass('filter__selected')) {
-
-                    $('.main__title').text('My all projects');
-                    $(this).removeClass('filter__selected');
-                    // console.log('remove class from active button');
-                    $('.project').removeClass('remove-project');
-                    //console.log('show all projects');
-                }
-                else {
-                    $('.main__title').text('My projects using ' + $(this).text());
-                    filterTags.removeClass('filter__selected');
-                    // console.log('remove class .filter__selected everywhere');
-                    $(this).addClass('filter__selected');
-                    // console.log('add class filter__selected on that button');
-                    $('.project').addClass('remove-project');
-                    //console.log('remove all projects');
-                    var tags = project.tags;
-                    tags.forEach(function (tag) {
-                        console.log(tag);
-                        if ($(that).attr('id') === tag) {
-                            console.log('rovnaji se');
-                            //  ==> show all projects with that specific tag
-                        }
-                        else {
-
-                            console.log('nerovnaji se');
-                        }
-                    })
-                }
-            });
 
         });
+        var filterTags = $('.filter__tag');
+        filterTags.click(function () {
+            var that = $(this);
+            if ($(this).hasClass('filter--selected')) {
 
+                $('.filter__title').text('All projects');
+                $(this).removeClass('filter--selected');
+                // console.log('remove class from active button');
+                $('.project').removeClass('project--hide');
+                //console.log('show all projects');
+            }
+            else {
+                $('.filter__title').text('Projects using ' + $(this).text());
+                filterTags.removeClass('filter--selected');
+                // console.log('remove class .filter__selected everywhere');
+                $(this).addClass('filter--selected');
+                // console.log('add class filter__selected on that button');
+                $('.project').addClass('project--hide');
+                //console.log('remove all projects');
+                // ==> add .map for tags and after .filter for projects with specific tag
+                if (that.attr('id') === tag) {
+                    console.log('rovnaji se');
+                    //  ==> show all projects with that specific tag
+                }
+                else {
+
+                    console.log('nerovnaji se');
+                }
+            }
+        });
 
     });
+
 
     var rotation = 0;
 
@@ -74,25 +73,7 @@ $(document).ready(function () {
 
     });
 
-    // var filterTags = $('.filter__tag');
-    //
-    // filterTags.click(function () {
-    //     if ($(this).hasClass('filter__selected')) {
-    //         $(this).removeClass('filter__selected');
-    //         console.log('remove class from active button');
-    //     }
-    //     else {
-    //         filterTags.removeClass('filter__selected');
-    //         console.log('remove class .filter__selected everywhere');
-    //         $(this).addClass('filter__selected');
-    //         console.log('add class filter__selected on that button');
-    //     }
-    //
-    //
-    //
-    // });
-
 });
 
 
-//https://api.github.com/users/zdenekpribyla/repos
+// api of gitHub projects: https://api.github.com/users/zdenekpribyla/repos
