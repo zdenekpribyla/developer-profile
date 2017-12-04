bf0b15264ef12d006f76ce9854b5e9ffa829aaa4
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-12-04 23:08:55 +0100

    add second url to project detail

--- js/app.js
+++ js/app.js
@@ -10,8 +10,9 @@ $(document).ready(function () {
                 '<div class="project">' +
                 '<h3 class="project__title">' + project.name + '</h3>' +
                 '<p class="project__description">' + project.description + '</p>' +
-                '<div class="project__detail"><span class="project__detail__span"><i class="fa fa-github"></i>  GitHub: </span> ' +
-                '<a href="' + project.html_url + '">' + project.html_url + '</a>' +
+                '<div class="project__detail"><div class="single__detail"><span class="project__detail__span"><i class="fa fa-github"></i>  GitHub: </span> ' +
+                '<a href="' + project.html_url + '">' + project.html_url + '</a></div>' + '<div class="single__detail"><span class="project__detail__span"><i class="fa fa-link"></i>  URL (live or demo version): </span> ' +
+                '<a href="' + project.demo_url + '">' + project.demo_url + '</a></div>' +
                 '</div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
                 '</div>');
 
@@ -34,8 +35,9 @@ $(document).ready(function () {
                         '<div class="project">' +
                         '<h3 class="project__title">' + project.name + '</h3>' +
                         '<p class="project__description">' + project.description + '</p>' +
-                        '<div class="project__detail"><span class="project__detail__span"><i class="fa fa-github"></i>  GitHub: </span> ' +
-                        '<a href="' + project.html_url + '">' + project.html_url + '</a>' +
+                        '<div class="project__detail"><div class="single__detail"><span class="project__detail__span"><i class="fa fa-github"></i>  GitHub: </span> ' +
+                        '<a href="' + project.html_url + '">' + project.html_url + '</a></div>' + '<div class="single__detail"><span class="project__detail__span"><i class="fa fa-link"></i>  URL (live or demo version): </span> ' +
+                        '<a href="' + project.demo_url + '">' + project.demo_url + '</a></div>' +
                         '</div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
                         '</div>');
 
@@ -63,8 +65,9 @@ $(document).ready(function () {
                                 '<div class="project">' +
                                 '<h3 class="project__title">' + singleProject.name + '</h3>' +
                                 '<p class="project__description">' + singleProject.description + '</p>' +
-                                '<div class="project__detail"><span class="project__detail__span"><i class="fa fa-github"></i>  GitHub: </span> ' +
-                                '<a href="' + singleProject.html_url + '">' + singleProject.html_url + '</a>' +
+                                '<div class="project__detail"><div class="single__detail"><span class="project__detail__span"><i class="fa fa-github"></i>  GitHub: </span> ' +
+                                '<a href="' + singleProject.html_url + '">' + singleProject.html_url + '</a></div>' + '<div class="single__detail"><span class="project__detail__span"><i class="fa fa-link"></i>  URL (live or demo version): </span> ' +
+                                '<a href="' + singleProject.demo_url + '">' + singleProject.demo_url + '</a></div>' +
                                 '</div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
                                 '</div>');
                         }
