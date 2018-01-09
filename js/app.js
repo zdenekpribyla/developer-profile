ba55d15d9f9a8f2c3d019cb1e8b8c41a5affcd7f
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2018-01-09 17:52:56 +0100

    improve naming in app.js

--- js/app.js
+++ js/app.js
@@ -7,45 +7,44 @@ var emptyStar = '<i class="fa fa-star-o fa-yellow" aria-hidden="true"></i>';
 var fullStar = '<i class="fa fa-star fa-yellow" aria-hidden="true"></i>';
 var halfStar = '<i class="fa fa-star-half-o fa-yellow" aria-hidden="true"></i>';
 
-var starsGenerator = function (number) {
-    var isInteger = number % 1 === 0;
+var footerStarsGenerator = function (number) {
+    var starRaitingIsInteger = number % 1 === 0;
     // console.log('isInteger', isInteger);
 
-    var result = '';
-    var resultEmpty = '';
+    var fullStarsNumber = '';
+    var emptyStarsNumber = '';
     var starsMaximumNumber = 5;
-    if (isInteger) {
+    if (starRaitingIsInteger) {
 
-        for (var i = 0; i < number; i++) {
-            result = result + fullStar
+        for (var a = 0; a < number; a++) {
+            fullStarsNumber = fullStarsNumber + fullStar
         }
-        for (var i = 0; i < (starsMaximumNumber - number); i++) {
-            resultEmpty = resultEmpty + emptyStar
+        for (var b = 0; b < (starsMaximumNumber - number); b++) {
+            emptyStarsNumber = emptyStarsNumber + emptyStar
         }
-        return result + resultEmpty
+        return fullStarsNumber + emptyStarsNumber
     }
 
     else {
 
-        for (var i = 0; (i + 1) < number; i++) {
-            result = result + fullStar
+        for (var c = 0; (c + 1) < number; c++) {
+            fullStarsNumber = fullStarsNumber + fullStar
         }
         var roundNumber = Math.ceil(number);
-
-        for (var i = 0; i < (starsMaximumNumber - roundNumber); i++) {
-            resultEmpty = resultEmpty + emptyStar
+        for (var d = 0; d < (starsMaximumNumber - roundNumber); d++) {
+            emptyStarsNumber = emptyStarsNumber + emptyStar
         }
-        return result + halfStar + resultEmpty
+        return fullStarsNumber + halfStar + emptyStarsNumber
     }
 };
 
-var tagsGenerator = function (tags, className) {
+var filterAndProjectTagsGenerator = function (tags, className) {
+
     var projectTags = '';
-
     tags.forEach(function (singleTag) {
-        console.log('tag: ' + singleTag); // az sem to je dobre
-
-        console.log('type of tags', typeof singleTag);
+        // console.log('tag: ' + singleTag); // az sem to je dobre
+        //
+        // console.log('type of tags', typeof singleTag);
 
         if (typeof singleTag === 'object') {
             singleTag = singleTag.name
@@ -58,29 +57,32 @@ var tagsGenerator = function (tags, className) {
 
 var projectDetailUrlGenerator = function (projectDetailClassName, iconClassName, titleClassName, titleText, linkClassName, url) {
 
-    var resultUrlGenerator;
+    var typeOfUrlResult;
     if (url === ""
         || typeof url === "object"
         || typeof url === "boolean"
         || typeof url === "function"
         || typeof url === "number"
         || typeof url === "undefined") {
-        resultUrlGenerator = "";
+
+        typeOfUrlResult = "";
     }
 
     else if (url.indexOf("http") >= 0) {  //possible to use => url.include("http") === true
-        resultUrlGenerator = '<div class="' + projectDetailClassName + '"><i class="fa ' + iconClassName + '"></i><span class="'
+
+        typeOfUrlResult = '<div class="' + projectDetailClassName + '"><i class="fa ' + iconClassName + '"></i><span class="'
             + titleClassName + '">' + titleText + ': </span><span class="' + linkClassName + '"><a target="_blank" href="' + url + '">' +
             url + '</a></span></div>';
     }
 
     else {
-        resultUrlGenerator = '<div class="' + projectDetailClassName + '"><i class="fa ' + iconClassName + '"></i><span class="'
+
+        typeOfUrlResult = '<div class="' + projectDetailClassName + '"><i class="fa ' + iconClassName + '"></i><span class="'
             + titleClassName + '">' + titleText + ': </span><span class="' + linkClassName + '">private url</span></div>';
-        console.log('spustil se private url');
+        // console.log('spustil se private url');
     }
 
-    return resultUrlGenerator;
+    return typeOfUrlResult;
 
 };
 
@@ -89,33 +91,17 @@ $(document).ready(function () {
     $.get('./js/data.json/', function (data) {
         data.mySkills.forEach(function (skill) {
             console.log("mySkills");
-            //these are duplicity filter tags
-            // $('.filter__input-tag').append(
-            //     '<li class="filter__tag" id="' + skill.name + '">' + skill.name + '</li>'
-            // );
 
             $('.footer__input-star').append(
                 '<div class="">' + footerTagGenerator(skill.name, "footer__tag") + '<div class="footer__star">' +
-                starsGenerator(skill.rating) + '</div>'
+                footerStarsGenerator(skill.rating) + '</div>'
             )
 
-            // $('.footer__input-star').append(
-            //     '<li><div class=""><div class="footer__tag">' + skill.name + '</div><div class="footer__star">' +
-            //     starsGenerator(skill.rating) + '</div></li>'
-            // )
 
-            // origin code for footer skills and stars
-            // $('.footer__input-star').append(
-            //     '<li><div class=""><div class="footer__tag">' + skill.name + '</div><div class="footer__star">' +
-            //     '<ul class="ratingSM o' + skill.rating + 'star"><li class="one">1</li><li class="two">2</li>' +
-            //     '<li class="three">3</li><li class="four">4</li><li class="five">5</li></ul></div></div></li>'
-            // )
-
-
-        }); //konec forEach
+        });
 
         $('.filter__input-tag').append(
-            tagsGenerator(data.mySkills, 'filter__tag')
+            filterAndProjectTagsGenerator(data.mySkills, 'filter__tag')
         );
 
 
@@ -131,33 +117,11 @@ $(document).ready(function () {
                 '<div class="project__detail">' +
                 projectDetailUrlGenerator("single__detail", "fa-github-alt", "project__detail__span", " GitHub", "link", project.html_url) +
                 projectDetailUrlGenerator("single__detail", "fa-link", "project__detail__span", " Demo", "link", project.demo_url) +
-                '<div class="project_tag"><i class="fa fa-trophy"></i><span class="project__detail__span">  Used Skills  </span>' + tagsGenerator(project.tags, 'project__tag-single') + '</div></div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
+                '<div class="project_tag"><i class="fa fa-trophy"></i><span class="project__detail__span">  Used Skills  </span>' + filterAndProjectTagsGenerator(project.tags, 'project__tag-single') + '</div></div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
                 '</div>');
 
 
             console.log(project.tags);
-
-            // console.log('-----------------');
-            // console.log('nazev projektu: ' + project.name);
-            // $('.main__list').append(
-            //     '<div class="project">' +
-            //     '<h3 class="project__title">' + project.name + '</h3>' +
-            //     '<p class="project__description">' + project.description + '</p>' +
-            //     '<div class="project__detail"><div class="single__detail"><i class="fa fa-github-alt"></i><span class="project__detail__span">  GitHub: </span> ' +
-            //     '<span class="link"><a target="_blank" href="' + project.html_url + '">' + project.html_url + '</a></span></div>' + '<div class="single__detail"><i class="fa fa-link"></i><span class="project__detail__span">  Demo: </span> ' +
-            //     '<span class="link"><a target="_blank" class="link" href="' + project.demo_url + '">' + project.demo_url + '</a></span></div>' +
-            //     '<div class="project_tag"><i class="fa fa-github-alt"></i><span class="project__detail__span">  Used Skills  </span>' + tagsGenerator(project.tags, 'project__tag-single') + '</div></div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
-            //     '</div>');
-            // console.log(project.tags);
-
-
-            // pridava .hide--selector uplne kazdemu projektu misto jen tomu jednomu
-            // if (project.demo_url === null) {
-            //     $('.project__detail').addClass('hide--selector');
-            //     console.log("skryj project detail");
-            //
-            // }
-
 
         });
 
