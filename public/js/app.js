cb46888280871b70749c1d251f5564b2afc8944a
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2019-06-12 15:56:51 +0200

    add new scripts (dynamic  year number, trimm description,etc)

--- public/js/app.js
+++ public/js/app.js
@@ -106,24 +106,27 @@ $(document).ready(function () {
         );
 
 
-        // console.log(data);
+
         data.myProjects.forEach(function (project) {
+
+            // divide project description on two part
+            var prodesArray = (project.description).split(" ")
+            numberOfCutWords = 10  // change number of trimmed words
+            sliceA = prodesArray.slice(0, numberOfCutWords).join(' ')
+            sliceB = prodesArray.slice(numberOfCutWords + 1).join(' ')
+            divideProjectDescription = '<p class="project__description"><span class="project__description-a">' + sliceA + '</span><span class="project__description-three-dots">...</span><span class="project__description-b hide--selector">' + sliceB + '<span></p>'
+
 
             $('.main__list').append(
                 '<div class="project">' +
-                '<h3 class="project__title">' + project.name + '</h3>' +
-                '<p class="project__description">' + project.description + '</p>' +
+                '<h3 class="project__title">' + project.name + '</h3>' + divideProjectDescription +
                 '<div class="project__details">' +
                 projectDetailUrlGenerator("project__single-detail", "fa-github-alt", "project__single-detail__span", " GitHub", "link", project.html_url) +
                 projectDetailUrlGenerator("project__single-detail", "fa-link", "project__single-detail__span", " url", "link", project.demo_url) +
                 '<div class="project_tag"><i class="fa fa-trophy"></i><span class="project__single-detail__span">  Used Skills  </span>' + filterAndProjectTagsGenerator(project.tags, 'project__tag-single') + '</div></div>' + '<div class="project__btn"><i class="fa fa-caret-down project__btn-open fa-2x" aria-hidden="true"></i></div>' +
                 '</div>');
 
-
-            // console.log(project.tags);
-
         });
-
 
         var filterTags = $('.filter__tag');
         filterTags.click(function () {
@@ -137,11 +140,17 @@ $(document).ready(function () {
                 //console.log('show all projects');
 
                 data.myProjects.forEach(function (project) {
+                    // divide project description on two part
+                    var prodesArray = (project.description).split(" ")
+                    numberOfCutWords = 10  // change number of trimmed words
+                    sliceA = prodesArray.slice(0, numberOfCutWords).join(' ')
+                    sliceB = prodesArray.slice(numberOfCutWords + 1).join(' ')
+                    divideProjectDescription = '<p class="project__description"><span class="project__description-a">' + sliceA + '</span><span class="project__description-three-dots">...</span><span class="project__description-b hide--selector">' + sliceB + '<span></p>'
 
                     $('.main__list').append(
                         '<div class="project">' +
                         '<h3 class="project__title">' + project.name + '</h3>' +
-                        '<p class="project__description">' + project.description + '</p>' +
+                        divideProjectDescription +
                         '<div class="project__details">' +
                         projectDetailUrlGenerator("project__single-detail", "fa-github-alt", "project__single-detail__span", " GitHub", "link", project.html_url) +
                         projectDetailUrlGenerator("project__single-detail", "fa-link", "project__single-detail__span", " url", "link", project.demo_url) +
@@ -163,6 +172,12 @@ $(document).ready(function () {
 
                 //console.log(data);
                 data.myProjects.forEach(function (project) {
+                    // divide project description on two part
+                    var prodesArray = (project.description).split(" ")
+                    numberOfCutWords = 10  // change number of trimmed words
+                    sliceA = prodesArray.slice(0, numberOfCutWords).join(' ')
+                    sliceB = prodesArray.slice(numberOfCutWords + 1).join(' ')
+                    divideProjectDescription = '<p class="project__description"><span class="project__description-a">' + sliceA + '</span><span class="project__description-three-dots">...</span><span class="project__description-b hide--selector">' + sliceB + '<span></p>'
 
                     project.tags.forEach(function (singleTag) {
 
@@ -171,7 +186,7 @@ $(document).ready(function () {
                             $('.main__list').append(
                                 '<div class="project">' +
                                 '<h3 class="project__title">' + project.name + '</h3>' +
-                                '<p class="project__description">' + project.description + '</p>' +
+                                divideProjectDescription +
                                 '<div class="project__details">' +
                                 projectDetailUrlGenerator("project__single-detail", "fa-github-alt", "project__single-detail__span", " GitHub", "link", project.html_url) +
                                 projectDetailUrlGenerator("project__single-detail", "fa-link", "project__single-detail__span", " url", "link", project.demo_url) +
@@ -187,20 +202,43 @@ $(document).ready(function () {
     });
 
 
-// alternative solution: https://stackoverflow.com/questions/1359018/in-jquery-how-to-attach-events-to-dynamic-html-elements
+    // alternative solution: https://stackoverflow.com/questions/1359018/in-jquery-how-to-attach-events-to-dynamic-html-elements
     $('.main__list').on('click', '.project__btn', function () {
         // console.log('click button');
+
+        var projectDetailA = $(this).parent().find('.project__description-a');  // s tim zatim nic nedelam
+
+        var projectDetailDots = $(this).parent().find('.project__description-three-dots');
+        // if (projectDetailDots.hasClass('hide--selector')) {
+        //     $(projectDetailDots).removeClass('hide--selector');
+        //     }
+        // else {
+        //     $(projectDetailDots).addClass('hide--selector');
+        //     $(projectDetailDots).append(' hello ')
+        // }
+
+        var projectDetailB = $(this).parent().find('.project__description-b');
+        if (projectDetailB.hasClass('hide--selector')) {
+            $(projectDetailB).removeClass('hide--selector')
+            $(projectDetailDots).text(' ')
+        }
+        else {
+            $(projectDetailB).addClass('hide--selector');
+            $(projectDetailDots).text('...')
+        }
+
+
         var projectDetail = $(this).parent().find('.project__details');
         $(projectDetail).toggle(450);
-
 
         var that = $(this);
         setTimeout(function () {
             if (that.hasClass('project__btn-rotate')) {
-                that.removeClass('project__btn-rotate').animate({opacity: '0.4'}, "slow").animate({opacity: '1'}, 150);
+                that.removeClass('project__btn-rotate').animate({ opacity: '0.4' }, "slow").animate({ opacity: '1' }, 150);
+
             }
             else {
-                that.addClass('project__btn-rotate').animate({opacity: '0.4'}, "slow").animate({opacity: '1'}, 150);
+                that.addClass('project__btn-rotate').animate({ opacity: '0.4' }, "slow").animate({ opacity: '1' }, 150)
             }
         }, 300)
 
@@ -208,7 +246,7 @@ $(document).ready(function () {
     });
 
 
-//Check to see if the window is top if not then display button
+    //Check to see if the window is top if not then display button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.scroll-to-top').fadeIn();
@@ -217,21 +255,36 @@ $(document).ready(function () {
         }
     });
 
-//Click event to scroll to top
+    //Click event to scroll to top
     $('.scroll-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 800);
+        $('html, body').animate({ scrollTop: 0 }, 800);
         return false;
     });
 
+    //Script generates current year at web footer
+    var getCurrentYear = function () {
+        var fullDate = new Date;
+        var onlyYear = fullDate.getFullYear();
+        document.getElementById("current-year").innerHTML = onlyYear
+    }
+    getCurrentYear()
+
+
+
+
+
+
+
+
 
     console.log("%cHi Developer,\n\nI am glad to see you watch my code.\n\nDo not hesitate to get me a feedback via %czdenek.pribyla@gmail.com%c\n\nSee you", "color: #222", "color: #2d6986; font-weight: bold", "color: #222");
-    console.log('            _ _           _                            \n' +
-        '           (_) |         | |                           \n' +
-        ' _ __  _ __ _| |__  _   _| | __ _   ___ ___  _ __ ___  \n' +
-        '| \'_ \\| \'__| | \'_ \\| | | | |/ _` | / __/ _ \\| \'_ ` _ \\ \n' +
-        '| |_) | |  | | |_) | |_| | | (_| || (_| (_) | | | | | |\n' +
-        '| .__/|_|  |_|_.__/ \\__, |_|\\__,_(_)___\\___/|_| |_| |_|\n' +
-        '| |                  __/ |                             \n' +
-        '|_|                 |___/                              ')
+    // console.log('            _ _           _                            \n' +
+    //     '           (_) |         | |                           \n' +
+    //     ' _ __  _ __ _| |__  _   _| | __ _   ___ ___  _ __ ___  \n' +
+    //     '| \'_ \\| \'__| | \'_ \\| | | | |/ _` | / __/ _ \\| \'_ ` _ \\ \n' +
+    //     '| |_) | |  | | |_) | |_| | | (_| || (_| (_) | | | | | |\n' +
+    //     '| .__/|_|  |_|_.__/ \\__, |_|\\__,_(_)___\\___/|_| |_| |_|\n' +
+    //     '| |                  __/ |                             \n' +
+    //     '|_|                 |___/                              ')
 
 });
